aa9cdd3b4ff81e31d91185759bc1a00888e722f5
senuri perera <pereralsd@students.nsbm.ac.lk>
2025-02-18 16:49:03 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dashbard.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 void main() async {
